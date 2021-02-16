1d3f50d3529cc309b5d708c554b77cf894d11162
jay-web <jay.developer12@gmail.com>
2021-02-16 14:00:43 +0530

    Updates

---
+++ precache-manifest.3caf45cd658426b423d3415e391cfc2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a85265a983bf097a68d3f49c79ce1d",
+    "url": "/jscoder/index.html"
+  },
+  {
+    "revision": "0d98d19970f7df69eade",
+    "url": "/jscoder/static/css/main.815a0369.chunk.css"
+  },
+  {
+    "revision": "136bdb5447e5b52c3319",
+    "url": "/jscoder/static/js/2.24914e7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jscoder/static/js/2.24914e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55dd6b634416b8ad8bb9",
+    "url": "/jscoder/static/js/3.b95d801f.chunk.js"
+  },
+  {
+    "revision": "0d98d19970f7df69eade",
+    "url": "/jscoder/static/js/main.844fecf3.chunk.js"
+  },
+  {
+    "revision": "092055d2c9120ba5db78",
+    "url": "/jscoder/static/js/runtime-main.458734bb.js"
+  }
+]);
